7315546da48bd06d2f6b497c6165acfe82099e69
Vikas Kumar <vsrathod39@gmail.com>
2022-01-17 15:05:35 +0530

    Navbar created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const links = [
+    {
+      to: "/",
+      title: "Home",
+      id: 1,
+    },
+    {
+      to: "/login",
+      title: "Login",
+      id: 2,
+    },
+    {
+      to: "/dashboard",
+      title: "Dashboard",
+      id: 3,
+    },
+    // {
+    //   to: "/login/admin",
+    //   title: "Login as admin",
+    // },
+  ];
+  return (
+    <div>
+      {links.map((e) => {
+        return (
+          <Link key={e.id} to={e.to}>
+            {e.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
