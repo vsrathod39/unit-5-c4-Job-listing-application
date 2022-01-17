d144b1db6d8761766150c811a4c8481a3daf9256
Vikas Kumar <vsrathod39@gmail.com>
2022-01-17 18:06:56 +0530

    logout added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -61,7 +61,9 @@ export default function Navbar() {
           <Link to="/user/dashboard">Dashboard</Link>
           <button onClick={handleLogout}>Logout</button>
         </>
-      ) : null}
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
     </div>
   );
 }
